5078bb671d640756e858176577186a74f0390e62
Olgane <meuzardgaelle@hotmail.fr>
2016-05-23 11:37:09 +0200

    ajout pages + datatables

--- src/AMAPBundle/Controller/DefaultController.php
+++ src/AMAPBundle/Controller/DefaultController.php
@@ -10,8 +10,20 @@ class DefaultController extends Controller
     {
         return $this->render('AMAPBundle:Default:index.html.twig');
     }
-    public function accueilAction()
+    public function producteurAction()
     {
-        return $this->render('AMAPBundle:Default:accueil.html.twig');
+        return $this->render('AMAPBundle:Default:producteur.html.twig');
+    }
+	public function actualitesAction()
+    {
+        return $this->render('AMAPBundle:Default:actualites.html.twig');
+    }
+	public function amapAction()
+    {
+        return $this->render('AMAPBundle:Default:amap.html.twig');
+    }
+	public function produitAction()
+    {
+        return $this->render('AMAPBundle:Default:produit.html.twig');
     }
 }
