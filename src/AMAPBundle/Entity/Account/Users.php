4caf2a7162abaaba1b343c63a817f89c18ef95f7
Bastien36 <bastienderlon@gmail.com>
2016-05-30 22:01:24 +0200

    Classe Utilisateur (FOSUserBundle)
    Mise en place de la classe utilisateur

---
+++ src/AMAPBundle/Entity/Account/Users.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AMAPBundle\Entity\Account;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ */
+class Users extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
