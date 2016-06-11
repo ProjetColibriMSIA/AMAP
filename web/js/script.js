7639921b3bef20d9250f1281af44e6c4d10ece38
maxenceblais <maxencehand@hotmail.fr>
2016-06-11 18:23:07 +0200

    Commit du bootstrap
    On a le menu en responsive et la structure en grille a suivre.
    Si vous ne connaissez pas la technique de grille pour vos parties, aller
    voir sur le site .
    https://openclassrooms.com/courses/prenez-en-main-bootstrap/une-grille
    c'est plutôt bien expliqué.

--- web/js/script.js
+++ web/js/script.js
@@ -1,66 +1,18 @@
 /*$(document).ready(function() { 
-	$(".list-services .tooltips").easyTooltip();
+    $(".list-services .tooltips").easyTooltip();
 }); */
 
 $(window).load(function() {
 
-	$('#bgSlider').bgSlider({
-		duration:1000,
-		pagination:'.pagination',
-		preload:true,
-		slideshow:10000,
-		spinner:'.bg_spinner'
-	});
-
-    // En cas de redimensionnement de la fenêtre
-    $(window).resize(function(){ 
-        redimensionnement(); 
-    }); 
-	
-	
-	
-
-//Fermeture de la pop-up et du fond
-$('a.close, #fade').live('click', function() { //Au clic sur le bouton ou sur le calque...
-	$('#fade , .popup_block').fadeOut(function() {
-		$('#fade, a.close').remove();  //...ils disparaissent ensemble
-	});
-	return false;
-});
-	
-	
 });
 
+$(document).ready(function () {
+    $('#example').DataTable({
+    });
+});
 
+//on fait 1000 pour 1s *20pour changer toutes les 20s
+$('.carousel').carousel({
+    interval: 1000*20
+});
 
-
-function redimensionnement(){ 
- 
-    var $image = $('#bgSlider img');
-    var image_width = $image.width(); 
-    var image_height = $image.height();     
-     
-    var over = image_width / image_height; 
-    var under = image_height / image_width; 
-     
-    var body_width = $(window).width(); 
-    var body_height = $(window).height(); 
-     
-    if (body_width / body_height >= over) { 
-      $image.css({ 
-        'width': body_width + 'px', 
-        'height': Math.ceil(under * body_width) + 'px', 
-        'left': '0px', 
-        'top': Math.abs((under * body_width) - body_height) / -2 + 'px' 
-      }); 
-    }  
-     
-    else { 
-      $image.css({ 
-        'width': Math.ceil(over * body_height) + 'px', 
-        'height': body_height + 'px', 
-        'top': '0px', 
-        'left': Math.abs((over * body_height) - body_width) / -2 + 'px' 
-      }); 
-    } 
-} 
