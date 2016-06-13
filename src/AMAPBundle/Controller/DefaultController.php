8f1ac83d435fc6bbbfc349b906a48f41401e7cef
Olgane <meuzardgaelle@hotmail.fr>
2016-06-13 16:05:49 +0200

    registrer

--- src/AMAPBundle/Controller/DefaultController.php
+++ src/AMAPBundle/Controller/DefaultController.php
@@ -2,8 +2,13 @@
 
 namespace AMAPBundle\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Form\UserType;
+use AppBundle\Entity\Account\User;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class DefaultController extends Controller {
 
@@ -76,5 +81,41 @@ class DefaultController extends Controller {
 
         return $this->render('AMAPBundle:Default:product.html.twig', array('products' => $products));
     }
+	
+	  /**
+     * @Route("/register")
+     */
+    public function registerAction(Request $request)
+    {
+        // 1) build the form
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
 
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // 3) Encode the password (you could also do this via Doctrine listener)
+            $password = $this->get('security.password_encoder')
+                ->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+
+            // 4) save the User!
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+
+            return $this->redirectToRoute('accueil');
+        }
+
+        return $this->render(
+            'registration/register.html.twig',
+            array('form' => $form->createView())
+        );
+    }
+    
+	
 }
