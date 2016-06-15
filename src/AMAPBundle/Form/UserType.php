ef6fcea57c0571113ea68b08aaf5f63378cf26e2
Bastien36 <bastienderlon@gmail.com>
2016-06-15 16:40:11 +0200

    Commit Translator + Modif Register

--- src/AMAPBundle/Form/UserType.php
+++ src/AMAPBundle/Form/UserType.php
@@ -19,26 +19,31 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-			->add('name', TextType::class, array('label_attr' => array('class' => 'control-label col-sm-5'), 'attr'  => array('class' => 'form-control width300')))
-			->add('firstname', TextType::class, array('label_attr' => array('class' => 'control-label col-sm-5'), 'attr'  => array('class' => 'form-control width300')))
-			->add('email', EmailType::class, array('label_attr' => array('class' => 'control-label col-sm-5'), 'attr'  => array('class' => 'form-control width300')))
-			->add('adress', TextareaType::class, array('label_attr' => array('class' => 'control-label col-sm-5'), 'attr'  => array('class' => 'form-control width300')))
-			->add('locale', TextType::class, array('label_attr' => array('class' => 'control-label col-sm-5'), 'attr'  => array('class' => 'form-control width300')))
-            ->add('username', TextType::class, array('label_attr' => array('class' => 'control-label col-sm-5'), 'attr'  => array('class' => 'form-control width300')))
-            ->add('plainPassword', RepeatedType::class,
+                        ->add('username', TextType::class, array('label_attr' => array('class' => 'control-label col-sm-5'), 'attr'  => array('class' => 'form-control width300'),'label_format'=>'label_username'))
+                        ->add('plainPassword', RepeatedType::class,
 			array(
                 'type' => PasswordType::class,
 				'options' => array('label_attr' => array('class' => 'control-label col-sm-5'),'attr' => array('class' => 'form-control width300')),
-                'first_options'  => array('label' => 'Password'),
-                'second_options' => array('label' => 'Repeat Password')
-            ));			
+                'first_options'  => array('label' => 'label_password'),
+                'second_options' => array('label' => 'label_confirmpassword')
+            ))
+                        ->add('email', EmailType::class, array('label_attr' => array('class' => 'control-label col-sm-5'), 'attr'  => array('class' => 'form-control width300'),'label_format'=>'label_email'))	
+                        ->add('name', TextType::class, array('label_attr' => array('class' => 'control-label col-sm-5'), 'attr'  => array('class' => 'form-control width300'),'label_format'=>'label_name'))
+			->add('firstname', TextType::class, array('label_attr' => array('class' => 'control-label col-sm-5'), 'attr'  => array('class' => 'form-control width300'),'label_format'=>'label_firstname'))
+			->add('adress', TextareaType::class, array('label_attr' => array('class' => 'control-label col-sm-5'), 'attr'  => array('class' => 'form-control width300'),'label_format'=>'label_adress'))
+			->add('locale', TextType::class, array('label_attr' => array('class' => 'control-label col-sm-5'), 'attr'  => array('class' => 'form-control width300'),'label_format'=>'label_locale'))
+          
+           ;			
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
             'data_class' => 'AMAPBundle\Entity\Account\User',
+            'translation_domain' => 'AMAPBundle'
         ));
     }
+    
+
 }
 ?>
